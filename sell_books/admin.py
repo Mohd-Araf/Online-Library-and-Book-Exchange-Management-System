8c46af46abe9c00ce9cd7b0bd015a7b2166ce6d6
MOHD Araf <157881351+Mohd-Araf@users.noreply.github.com>
2025-10-18 03:15:14 +0600

    update

--- sell_books/admin.py
+++ sell_books/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import BookType, SellBook
+from .models import BookType, SellBook, PurchasedBook
+
 
 @admin.register(BookType)
 class BookTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
+
 
 @admin.register(SellBook)
 class SellBookAdmin(admin.ModelAdmin):
@@ -17,3 +19,19 @@ class SellBookAdmin(admin.ModelAdmin):
         if not obj.user:
             obj.user = request.user
         obj.save()
+
+
+@admin.register(PurchasedBook)
+class PurchasedBookAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book_name', 'author_name', 'price')
+    search_fields = ('user__username', 'book__name', 'book__author')
+    list_filter = ('user',)
+
+    def book_name(self, obj):
+        return obj.book.name
+
+    def author_name(self, obj):
+        return obj.book.author
+
+    def price(self, obj):
+        return obj.book.price
